6cf0f7a2f68bfe0d1c730aee814ce87dec9e24fa
Sergey Afonin <afoninsb@yandex.ru>
2023-07-05 08:25:27 +0000

    urls

--- company/api/urls.py
+++ company/api/urls.py
@@ -1,30 +1,24 @@
 from django.urls import include, path
+from api.views.departments import DepartmentsViewSet
+from api.views.positions import PositionsViewSet
+from api.views.users import UsersViewSet
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework.routers import SimpleRouter
 
-# from api.views.ingredients import IngredientsViewSet
-# from api.views.recipes import RecipesViewSet
-# from api.views.tags import TagsViewSet
-# from api.views.users import UsersViewSet
 
 app_name = 'api'
 
 router = SimpleRouter()
 
-# router.register('ingredients', IngredientsViewSet, basename='ingredients')
-# router.register('recipes', RecipesViewSet, basename='recipes')
-# router.register('tags', TagsViewSet, basename='tags')
-# router.register('users', UsersViewSet, basename='users')
+router.register('departments', DepartmentsViewSet, basename='departments')
+router.register('positions', PositionsViewSet, basename='positions')
+router.register('users', UsersViewSet, basename='users')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
     path(
         'users/set_password/',
         DjoserUserViewSet.as_view({'post': 'set_password'})
-    ),
-    path(
-        'users/me/',
-        DjoserUserViewSet.as_view({'get': 'me'})
     ),
     path('', include(router.urls)),
 ]
