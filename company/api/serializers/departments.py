8faae99de5abcefc3c5e29836413a82f6cdbc02d
Sergey Afonin <afoninsb@yandex.ru>
2023-07-05 05:42:12 +0000

    DepartmentSerializer & PositionSerializer

--- company/api/serializers/departments.py
+++ company/api/serializers/departments.py
@@ -1,11 +1,22 @@
+from api.serializers.users import DirectorSerializer
 from rest_framework import serializers
-
+from django.db.models import Sum
 from departments.models import Department
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
     """Сериализатор модели Department."""
 
+    director = DirectorSerializer(read_only=True)
+
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = ('id', 'name', 'director')
+        read_only_fields = ('name', 'director')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['count_employees'] = instance.employees.count()
+        obj = instance.employees.aggregate(total_salary=Sum('salary'))
+        representation['total_salary'] = obj.total_salary
+        return representation
--- company/api/serializers/users.py
+++ company/api/serializers/users.py
@@ -6,8 +6,11 @@ from users.models import Employee
 class DirectorSerializer(serializers.ModelSerializer):
     """Сериализатор модели Employee для директора."""
 
+    photo = serializers.ImageField(use_url=True)
+
     class Meta:
         model = Employee
         fields = (
             'id', 'last_name', 'first_name', 'surname', 'photo',
         )
+        read_only_fields = ('last_name', 'first_name', 'surname', 'photo')
