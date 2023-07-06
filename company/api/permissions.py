120603ad428ce2aaf32cbc50ec381c1afa6fd178
Sergey Afonin <afoninsb@yandex.ru>
2023-07-06 13:02:47 +0000

    3

--- company/api/permissions.py
+++ company/api/permissions.py
@@ -5,4 +5,4 @@ class IsStaff(permissions.BasePermission):
     """Если админ, можно изменять список сотрудников."""
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user.is_staff is True
+        return request.user.is_authenticated and request.user.is_staff
